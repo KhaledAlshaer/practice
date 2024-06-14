e2183aae5e2b5636ad9a8d9aa4aeffca3816cf49
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-06-14 01:51:16 -0700

    Practice

---
+++ test.c
@@ -0,0 +1,30 @@
+#include "header.h"
+
+
+typedef enum {
+        SemiColon,
+	OPEN_PAREN,
+	CLOSE_PAREN,
+	OPEN_CURLY_PAREN,
+	CLOSE_CURLY_PAREN,
+} Seperators;
+
+typedef enum{
+	RETURN,
+	INT,
+} Keywords;
+
+
+typedef enum {
+	LITERAL_INT,
+} Literals;
+
+
+/**
+ *
+ * comment section
+ *
+ */
+int main() {
+	return (2);
+}
