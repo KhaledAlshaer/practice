df0f68e740a1f0b1165e9788c8be4732b4fdd170
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-06-15 07:57:15 -0700

    Practice

--- header.h
+++ header.h
@@ -4,4 +4,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+        SemiColon,
+	OPEN_PAREN,
+	CLOSE_PAREN,
+	OPEN_CURLY_PAREN,
+	CLOSE_CURLY_PAREN,
+    UNKNOWN,
+} Seperators;
+
+typedef enum{
+	RETURN,
+	INT,
+} Keywords;
+
+
+typedef enum {
+	LITERAL_INT,
+} Literals;
+
+typedef enum {
+    TOKEN_SEMICOLON,
+    TOKEN_OPEN_PAREN,
+    TOKEN_CLOSE_PAREN,
+    TOKEN_OPEN_CURLY_PAREN,
+    TOKEN_CLOSE_CURLY_PAREN,
+    TOKEN_RETURN,
+    TOKEN_INT,
+    TOKEN_LITERAL_INT,
+    TOKEN_UNKNOWN,
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char *value;
+} Token;
+
 #endif
--- test.c
+++ test.c
@@ -1,25 +1,5 @@
 #include "header.h"
 
-
-typedef enum {
-        SemiColon,
-	OPEN_PAREN,
-	CLOSE_PAREN,
-	OPEN_CURLY_PAREN,
-	CLOSE_CURLY_PAREN,
-} Seperators;
-
-typedef enum{
-	RETURN,
-	INT,
-} Keywords;
-
-
-typedef enum {
-	LITERAL_INT,
-} Literals;
-
-
 /**
  *
  * comment section
