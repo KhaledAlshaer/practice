4f3c436a5d33c95fd7f67468c5f2d102ca87ab1d
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-06-17 10:20:37 -0700

    Practice

--- lexer.c
+++ lexer.c
@@ -1,90 +1,98 @@
 #include "header.h"
 
 /**
- * 
- * 
- * comments section
- * 
- * 
+ *lexer - A Lexer Function For Our C Compiler
+ *@file: Source File
+ *Return: Returns The Token To Be Used Next By The Parser
  */
-int lexer(FILE *file) {
+Token lexer(FILE *file)
+{
+	char word[256];
+	int ch;
+	int i =  0;
+	Token token;
 
-    char word[256];
-    int ch;
-    int i =  0;
-    Token token;
-    token.value = NULL;
+	token.value = NULL;
+	if (file == NULL)
+	{
+		perror("File Doesn't Exist");
+		token.type = UNKNOWN;
+		return (token);
+	}
 
-    if (file == NULL){
-        perror("File Doesn't Exist");
-        token.type = UNKNOWN;
-        return(token);
-    }
+	while ((ch = fgetc(file)) != EOF)
+	{
+		switch (ch)
+		{
+			case ';':
+				token.type = TOKEN_SEMICOLON;
+				return (token);
+			case '(':
+				token.type = TOKEN_OPEN_PAREN;
+				return (token);
+			case ')':
+				token.type = TOKEN_CLOSE_PAREN;
+				return (token);
+			case '{':
+				token.type = TOKEN_OPEN_CURLY_PAREN;
+				return (token);
+			case '}':
+				token.type = TOKEN_CLOSE_CURLY_PAREN;
+				return (token);
+			default:
+				if (isalpha(ch))
+				{
+					word[i++] = ch;
 
-    while ((ch = fgetc(file)) != EOF) {
-        switch (ch) {
-            case ';':
-                token.type = TOKEN_SEMICOLON;
-                return (token);
-            case '(':
-                token.type = TOKEN_OPEN_PAREN;
-                return (token);
-            case ')':
-                token.type = TOKEN_CLOSE_PAREN;
-                return (token);
-            case '{':
-                token.type = TOKEN_OPEN_CURLY_PAREN;
-                return (token);
-            case '}':
-                token.type = TOKEN_CLOSE_CURLY_PAREN;
-                return (token);
-            default:
-                if (isalpha(ch)){
-                    word[i++] = ch;
+					while (isalpha(ch = fgetc(file)))
+					{
+						word[i++] = ch;
+					}
 
-                    while (isalpha(ch = fgetc(file))) {
-                        word[i++] = ch;
-                    }
+					ungetc(ch, file);
+					word[i] = '\0';
+					if (strcmp(word) == "return")
+					{
+						token.type = TOKEN_RETURN;
+					} else if (strcmp(word) == "int")
+					{
+						token.type = TOKEN_INT;
+					} else
+					{
+						token.type = TOKEN_UNKNOWN;
+					}
 
-                    ungetc(ch, file);
-                    word[i] = '\0';
-                    if (strcmp(word) == "return") {
-                        token.type = TOKEN_RETURN;
-                    } else if (strcmp(word) == "int") {
-                        token.type = TOKEN_INT;
-                    } else {
-                        token.type = TOKEN_UNKNOWN;
-                    }
+					token.value = strdup(word);
 
-                    token.value = strdup(word);
+					return (token);
+				} else if (isdigit(ch))
+				{
+					word[i++] = ch;
 
-                    return(token);
+					while (isdigit(ch))
+					{
+						word[i++] = ch;
+					}
 
+					ungetc(ch, file);
+					word[i] = '\0';
+					token.type = TOKEN_LITERAL_INT;
+					token.value = strdup(word);
 
-                } else if (isdigit(ch)) {
-                    word[i++] = ch;
+					return (token);
+				} else if (isspace(ch))
+				{
+					continue;
+				} else
+				{
+					token.type = TOKEN_UNKNOWN;
 
-                    while (isdigit(ch)) {
-                        word[i++] = ch;
-                    }
+					return (token);
+				}
+		}
+	}
 
-                    ungetc(ch, file);
-                    word[i] = '\0';
-                    token.type = TOKEN_LITERAL_INT;
-                    token.value = strdup(word);
+	token.type = TOKEN_UNKNOWN;
 
-                    return(token);
-                } else if (isspace(ch)) {
-                    continue;
-                } else {
-                    token.type = TOKEN_UNKNOWN;
-
-                    return(token);
-                }
-        }
-    }
-
-    token.type = TOKEN_UNKNOWN;
-
-    return(token);
+	return (token);
 }
