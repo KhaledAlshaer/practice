7fe1bccabfaa431c07da635c3318cb9f59a6cc89
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-07-19 10:33:40 -0700

    My First Script

--- commit.sh
+++ commit.sh
@@ -4,5 +4,6 @@
 if [ $# -ne 1 ]
 then
 	echo "this script needs exactly one argument (commit message) please rerun the script with your message"
+else
+	git add .&&git commit -m "$1"&&git push
 fi
-git add .&&git commit -m "$1"&&git push
